3d3b0e621d697b613849ed8d45fac5241ab9f931
MiningCryptoLive <celldr69@gmail.com>
2023-09-25 14:56:35 -0400

    Update alphaxcore.js

--- assets/js/alphaxcore.js
+++ assets/js/alphaxcore.js
@@ -354,7 +354,7 @@ function loadWidgetMediumPage() {
 		poolCoinWidgetMediumTemplate += "<div class='col-md-12'>";
 		poolCoinWidgetMediumTemplate += "<div class='med-box med-box-widget med-widget-user'>";
 		poolCoinWidgetMediumTemplate += "<div class='med-widget-user-header bg-night'>";
-		poolCoinWidgetMediumTemplate += "<button class='btn btn-outline-success btn-round btn-md float-right'><a href='#" + value.id.toLowerCase() + "'<span class='btn-label'>GO MINE&nbsp;&nbsp;<img src='coinlogo/" + value.coin.type.toLowerCase() + ".png' height='20' width='20'></span></a></button>";
+		poolCoinWidgetMediumTemplate += "<button class='btn btn-outline-success btn-round btn-md float-right'><a href='#" + value.id.toLowerCase() + "'<span class='btn-label'>GO MINE&nbsp;&nbsp;<img src='coinlogo/" + value.coin.type.toLowerCase() + ".png' height='20' width='20'></span></a></button>"; <br>
 		poolCoinWidgetMediumTemplate += "<h4 class='med-widget-user-username'>" + coinName + "</h4>";
                 poolCoinWidgetMediumTemplate += "</div>";
 		poolCoinWidgetMediumTemplate += "<div class='med-box-footer'>";
